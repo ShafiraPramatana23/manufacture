b93b780f612fd00bcbb604c87e7351eb3dcf402a
shafirapramatana <firapramatana03@gmail.com>
2021-07-19 22:35:20 +0700

    init project

---
+++ application/views/general/footer.php
@@ -0,0 +1,17 @@
+<!-- Footer-->
+<footer>
+        <!-- Copyright Section-->
+        <div class="copyright py-4 text-center text-white">
+            <div class="container"><small>Copyright &copy; Your Website 2021</small></div>
+        </div>
+        <!-- Bootstrap core JS-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="<?php echo base_url("assets/js/scripts.js")?>"></script>
+        <!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
+        <!-- * *                               SB Forms JS                               * *-->
+        <!-- * * Activate your form at https://startbootstrap.com/solution/contact-forms * *-->
+        <!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
+        <script src="https://cdn.startbootstrap.com/sb-forms-latest.js"></script>
+    </body>
+</html>
---
+++ application/views/main.php
@@ -0,0 +1,5 @@
+<?php
+$this->load->view('general/header.php');
+$this->load->view('content/'.$content);
+$this->load->view('general/footer.php');
+?>
